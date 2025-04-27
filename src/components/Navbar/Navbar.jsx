ad67edeb0d170be126a415f528b4be0250af2503
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-04-27 09:27:19 +0600

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { NavLink } from 'react-router';
+
+const Navbar = () => {
+    return (
+        <div>
+
+
+            <div className="navbar bg-base-100 shadow-sm">
+                <div className="navbar-start">
+
+                </div>
+                <div className="navbar-center">
+                    <ul className='flex gap-4 '>
+                        <NavLink to='/' className={({ isActive }) =>
+                            isActive ? "active text-blue-500 underline" : ""
+                        }>
+                            Home
+                        </NavLink>
+                        <NavLink to={'/login'} className={({ isActive }) =>
+                            isActive ? "active text-blue-500 underline" : ""
+                        }>
+                            Login
+                        </NavLink>
+                        <NavLink to={'/registration'} className={({ isActive }) =>
+                            isActive ? "active text-blue-500 underline" : ""
+                        }>
+                            Registration
+                        </NavLink>
+                    </ul>
+                </div>
+                <div className="navbar-end">
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/main.jsx
@@ -0,0 +1,41 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+import Root from './components/layout/Root.jsx';
+import Home from './components/Home/Home.jsx';
+import Login from './components/Login/Login.jsx';
+import Registration from './components/Registration/Registration.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+
+    children:[
+      {
+        index: true,
+        Component:Home
+      },
+      {
+        path:'login',
+        Component:Login
+      },
+      {
+        path:'registration',
+        Component:Registration
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <RouterProvider router={router} />
+  </StrictMode>,
+)
